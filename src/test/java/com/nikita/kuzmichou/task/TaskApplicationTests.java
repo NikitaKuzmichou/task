eca47790b5aa232b616231ceeb0ee6ee57d67fe1
NikitaKuzmichou <nekit338@gmail.com>
2020-12-09 22:48:55 +0300

    generated via spring initializr

---
+++ src/test/java/com/nikita/kuzmichou/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita.kuzmichou.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
